fa769ff4d6639464f1fd6d265c5dd521643852c2
“ablaizot” <ablaizot@bu.edu>
2023-04-14 15:03:14 -0400

    removing auto in function

--- speedtest.cxx
+++ speedtest.cxx
@@ -7,32 +7,32 @@
 
 //g++ -std=c++11 -Wall -rdynamic -lboost_system -lboost_filesystem speedtest.cxx -o speedtest
 
-void output_test(auto duration, uint32_t length){
-  // Prints tests to cmd
-  std::cout << "Time taken by loop: " << duration.count() << " milliseconds" << std::endl;
-	
-  double time_ms =std::chrono::duration<double>(duration).count();
-  	// writing a single word so 4 bytes
+void output_test(double time_ms, uint32_t length){
+  // Prints tests to cmd  
+	// writing a single word so 4 bytes
 	double bytes =  (double) length*WORD;
-  std::cout << "Number of words: " << length << "(%d)" <<bytes <<std::endl;
+	std::cout << "Number of words: " << length << "(%d)" <<bytes <<std::endl;
 	double bps =  bytes / (time_ms);
 	std::cout << "Duration: " << time_ms << std::endl;
 	std::cout << "Bytes per second: " << bps << "\n" <<std::endl;
 }
 
 void read_test(uint32_t length,uint32_t address, uint32_t * ptr){
-  // variable to assign read output
-  std::cout << "Running read test";
-  uint32_t read = 0;
-  auto start = std::chrono::high_resolution_clock::now();
+  	// variable to assign read output
+  	std::cout << "Running read test";
+  	uint32_t read = 0;
+  	auto start = std::chrono::high_resolution_clock::now();
 
 	//Speed test loop
 	for(uint32_t i = 0;i<length;i++){ 
 		read = ptr[address];
 	}
 
-  auto stop = std::chrono::high_resolution_clock::now();
-  output_test(std::chrono::duration_cast<std::chrono::milliseconds>(stop - start),length);
+  	auto stop = std::chrono::high_resolution_clock::now();
+  	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+  	double time_ms =std::chrono::duration<double>(duration).count();
+	std::cout << "Time taken by loop: " << duration.count() << " milliseconds" << std::endl;
+  	output_test(time_ms,length);
 }
 
 void write_test(uint32_t length, uint32_t address, uint32_t * ptr){
@@ -45,9 +45,11 @@ void write_test(uint32_t length, uint32_t address, uint32_t * ptr){
 		ptr[address] = i;
 	}
   
-  auto stop = std::chrono::high_resolution_clock::now();
-
-  output_test(std::chrono::duration_cast<std::chrono::milliseconds>(stop - start),length);
+  	auto stop = std::chrono::high_resolution_clock::now();
+  	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+  	double time_ms =std::chrono::duration<double>(duration).count();
+	std::cout << "Time taken by loop: " << duration.count() << " milliseconds" << std::endl;
+  	output_test(time_ms,length);
 }
 
 void write_read_test(uint32_t length, uint32_t address, uint32_t * ptr){
@@ -62,12 +64,14 @@ void write_read_test(uint32_t length, uint32_t address, uint32_t * ptr){
 		read = ptr[address];
 	}
   
-  auto stop = std::chrono::high_resolution_clock::now();
-
-  output_test(std::chrono::duration_cast<std::chrono::milliseconds>(stop - start),length);
+  	auto stop = std::chrono::high_resolution_clock::now();
+  	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+  	double time_ms =std::chrono::duration<double>(duration).count();
+	std::cout << "Time taken by loop: " << duration.count() << " milliseconds" << std::endl;
+  	output_test(time_ms,length);
 }
 
-void write_read_error_test(uint32_t length, uint32_t address, uint32_t ptr){
+void write_read_error_test(uint32_t length, uint32_t address, uint32_t * ptr){
   // variable to assign read output
   std::cout << "Running write test";
   uint32_t read = 0;
@@ -83,10 +87,12 @@ void write_read_error_test(uint32_t length, uint32_t address, uint32_t ptr){
 			error++;
 		}
 	}
-  
-  auto stop = std::chrono::high_resolution_clock::now();
-  std::cout << "Errors: " << error << std::endl;
-  output_test(std::chrono::duration_cast<std::chrono::milliseconds>(stop - start),length);
+  	std::cout << "Errors: " << error << std::endl;
+	auto stop = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+	double time_ms =std::chrono::duration<double>(duration).count();
+	std::cout << "Time taken by loop: " << duration.count() << " milliseconds" << std::endl;
+	output_test(time_ms,length);
 }
 
 int main(){
