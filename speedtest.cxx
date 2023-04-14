28db1fbc3540ca348e044d1d46720c19cb708a7c
ablaizot <ablaizot@bu.edu>
2023-04-13 20:03:10 -0400

    made tests into functions

--- speedtest.cxx
+++ speedtest.cxx
@@ -10,7 +10,10 @@
 
 #include "uioLabelFinder.hh"
 #include <sys/mman.h>
-//g++ -std=c++11 -Wall -rdynamic -lboost_system -lboost_filesystem -lpthread speedtest.cxx -o speedtest
+
+#define WORD 4 
+
+//g++ -std=c++11 -Wall -rdynamic -lboost_system -lboost_filesystem speedtest.cxx -o speedtest
 int main(){
 	uint32_t address= 0x000007F0;
 	uint32_t count = 1;
@@ -51,10 +54,83 @@ int main(){
 	
 	//length of loop
 	uint32_t length = 100000000;
-	auto start = std::chrono::high_resolution_clock::now();
 
-	//error counter
-	int error = 0;
+  read_test(length,address,ptr);
+  write_test(length,address,ptr);
+  write_read_test(length,address,ptr);
+  write_read_error_test(length,address,ptr);
+	
+	return 0;
+}
+
+void output_test(auto duration, uint32_t length){
+  // Prints tests to cmd
+  std::cout << "Time taken by loop: " << duration.count() << " milliseconds" << std::endl;
+	
+  double time_ms =std::chrono::duration<double>(duration).count();
+  	// writing a single word so 4 bytes
+	double bytes =  (double) length*WORD;
+  std::cout << "Number of words: " << length << "(%d)" <<bytes <<std::endl;
+	double bps =  bytes / (time_ms);
+	std::cout << "Duration: " << time_ms << std::endl;
+	std::cout << "Bytes per second: " << bps << "\n" <<std::endl;
+}
+
+void read_test(uint32_t length,uint32_t address, uint32_t ptr){
+  // variable to assign read output
+  std::cout << "Running read test";
+  uint32_t read = 0;
+  auto start = std::chrono::high_resolution_clock::now();
+
+	//Speed test loop
+	for(uint32_t i = 0;i<length;i++){ 
+		read = ptr[address];
+	}
+
+  auto stop = std::chrono::high_resolution_clock::now();
+  output_test(std::chrono::duration_cast<std::chrono::milliseconds>(stop - start),length);
+}
+
+void write_test(uint32_t length, uint32_t address, uint32_t ptr){
+  // variable to assign read output
+  std::cout << "Running write test";
+  auto start = std::chrono::high_resolution_clock::now();
+
+	//Speed test loop
+	for(uint32_t i = 0;i<length;i++){
+		ptr[address] = i;
+	}
+  
+  auto stop = std::chrono::high_resolution_clock::now();
+
+  output_test(std::chrono::duration_cast<std::chrono::milliseconds>(stop - start),length);
+}
+
+void write_read_test(uint32_t length, uint32_t address, uint32_t ptr){
+  // variable to assign read output
+  std::cout << "Running write test";
+  uint32_t read = 0;
+  auto start = std::chrono::high_resolution_clock::now();
+
+	//Speed test loop
+	for(uint32_t i = 0;i<length;i++){
+		ptr[address] = i;
+		read = ptr[address];
+	}
+  
+  auto stop = std::chrono::high_resolution_clock::now();
+
+  output_test(std::chrono::duration_cast<std::chrono::milliseconds>(stop - start),length);
+}
+
+void write_read_error_test(uint32_t length, uint32_t address, uint32_t ptr){
+  // variable to assign read output
+  std::cout << "Running write test";
+  uint32_t read = 0;
+  //error counter
+  int error = 0;
+
+  auto start = std::chrono::high_resolution_clock::now();
 
 	//Speed test loop
 	for(uint32_t i = 0;i<length;i++){
@@ -63,18 +139,11 @@ int main(){
 			error++;
 		}
 	}
-	
-	auto stop = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-	std::cout << "Time taken by loop: " << duration.count() << " milliseconds" << std::endl;
-	std::cout << "Number of words: " << length << std::endl;
-	std::cout << "Errors: " << error << std::endl;
-	double time_ms = std::chrono::duration<double>(duration).count();
-	// writing a single word so 4 bytes
-	double bytes =  (double) length*4;
-	double bps =  bytes / (time_ms);
-	std::cout << "Duration: " << time_ms << std::endl;
-	std::cout << "Bytes per second: " << bps << std::endl;
-	
-	return 0;
+  
+  auto stop = std::chrono::high_resolution_clock::now();
+  std::cout << "Errors: " << error << std::endl;
+  output_test(std::chrono::duration_cast<std::chrono::milliseconds>(stop - start),length);
 }
+
+
+
