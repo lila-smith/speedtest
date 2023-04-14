63a3cfcc13aeb15be09c7dc78524ee5733ee9571
“ablaizot” <ablaizot@bu.edu>
2023-04-14 14:42:19 -0400

    function position

--- speedtest.cxx
+++ speedtest.cxx
@@ -6,54 +6,6 @@
 
 
 //g++ -std=c++11 -Wall -rdynamic -lboost_system -lboost_filesystem speedtest.cxx -o speedtest
-int main(){
-	uint32_t address= 0x000007F0;
-	uint32_t count = 1;
-	char* UIO_DEBUG = getenv("UIO_DEBUG");
-
-	// found using ls /dev/uio*
-	std::string device_name  = "PL_MEM";
-
-	int uio = label2uio(device_name);
-
-
-	if(uio < 0){
-		// try the old version
-		if (NULL != UIO_DEBUG) {
-			printf("simple UIO finder failed, trying legacy\n");
-		}
-
-		uio = label2uio_old(device_name);
-		if (uio < 0) {
-			// at this point, old version has failed.
-			fprintf(stderr,"Device not found\n");
-			return 1;
-		}
-	}
-
-	char UIOFilename[] = "/dev/uioXXXXXXXXXX";
-	snprintf(UIOFilename,strlen(UIOFilename),
-		"/dev/uio%d",uio);
-
-	//Open UIO
-	int fdUIO = open(UIOFilename,O_RDWR);
-	if(fdUIO < 0){
-		fprintf(stderr,"Error");
-		return 1;
-	}
-
-	uint32_t * ptr = (uint32_t *) mmap(NULL,sizeof(uint32_t)*(address+count), PROT_READ|PROT_WRITE, MAP_SHARED, fdUIO,0x0);
-	
-	//length of loop
-	uint32_t length = 100000000;
-
-  read_test(length,address,ptr);
-  write_test(length,address,ptr);
-  write_read_test(length,address,ptr);
-  write_read_error_test(length,address,ptr);
-	
-	return 0;
-}
 
 void output_test(auto duration, uint32_t length){
   // Prints tests to cmd
@@ -137,5 +89,52 @@ void write_read_error_test(uint32_t length, uint32_t address, uint32_t ptr){
   output_test(std::chrono::duration_cast<std::chrono::milliseconds>(stop - start),length);
 }
 
+int main(){
+	uint32_t address= 0x000007F0;
+	uint32_t count = 1;
+	char* UIO_DEBUG = getenv("UIO_DEBUG");
 
+	// found using ls /dev/uio*
+	std::string device_name  = "PL_MEM";
+
+	int uio = label2uio(device_name);
+
+
+	if(uio < 0){
+		// try the old version
+		if (NULL != UIO_DEBUG) {
+			printf("simple UIO finder failed, trying legacy\n");
+		}
+
+		uio = label2uio_old(device_name);
+		if (uio < 0) {
+			// at this point, old version has failed.
+			fprintf(stderr,"Device not found\n");
+			return 1;
+		}
+	}
+
+	char UIOFilename[] = "/dev/uioXXXXXXXXXX";
+	snprintf(UIOFilename,strlen(UIOFilename),
+		"/dev/uio%d",uio);
+
+	//Open UIO
+	int fdUIO = open(UIOFilename,O_RDWR);
+	if(fdUIO < 0){
+		fprintf(stderr,"Error");
+		return 1;
+	}
+
+	uint32_t * ptr = (uint32_t *) mmap(NULL,sizeof(uint32_t)*(address+count), PROT_READ|PROT_WRITE, MAP_SHARED, fdUIO,0x0);
+	
+	//length of loop
+	uint32_t length = 100000000;
+
+  read_test(length,address,ptr);
+  write_test(length,address,ptr);
+  write_read_test(length,address,ptr);
+  write_read_error_test(length,address,ptr);
+	
+	return 0;
+}
 
