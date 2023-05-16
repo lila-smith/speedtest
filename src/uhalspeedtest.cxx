ae4675097b2468c388fd31b684a10f3ee25966b4
ablaizot <ablaizot@bu.edu>
2023-05-16 15:14:28 -0400

    reference

--- src/uhalspeedtest.cxx
+++ src/uhalspeedtest.cxx
@@ -6,7 +6,7 @@ int UHAL_TEST::uhalspeedtest(string reg, uint32_t loops)
 {
   uint32_t write_mem;
   uint32_t read_mem;
-  uhal::Node const & node;
+  uhal::Node const node;
   double speed;
 
   std::random_device rd;  //Will be used to obtain a seed for the random number engine
@@ -21,7 +21,7 @@ int UHAL_TEST::uhalspeedtest(string reg, uint32_t loops)
        << std::dec << loops << " loops doing write-read of incrementing 32-bit words to " << reg 
 	    << endl << endl; 
   //uhal::Node const & GetNode            (std::string const & reg);
-  node = SM->getNode(reg);
+  node = SM->GetNode(reg);
 
   for(uint32_t i = 0; i < loops; ++i) {
     //  uint32_t ReadNode                     (uhal::Node const & node);
