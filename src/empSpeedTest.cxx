29cdbf807a07ed482c11b956232e549acec4e12d
Aymeric Blaizot <ablaizot@bu.edu>
2024-06-20 16:36:39 +0200

    hardcoding file path

--- src/empSpeedTest.cxx
+++ src/empSpeedTest.cxx
@@ -30,7 +30,7 @@ int SPEED_TEST::empSpeedTest(string reg, uint64_t loops, string emp_connections_
   std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
   std::uniform_int_distribution<unsigned int> distrib(0, 0xFFFFFFFF);
 
-  const std::string lConnectionFilePath = emp_connections_file;
+  const std::string lConnectionFilePath = "/opt/address_table/emp_connections.xml";
   const std::string lDeviceId = "F1_IPBUS";
   const std::string lRegisterName = reg;
 
