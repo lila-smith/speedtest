f8327bddda2e94a01e61e9bf82c4290acab5c9c0
ablaizot <ablaizot@bu.edu>
2024-06-28 16:27:13 +0200

    updating speed calculations

--- src/test_helpers.cxx
+++ src/test_helpers.cxx
@@ -3,14 +3,14 @@
 void test_print(std::chrono::time_point<std::chrono::high_resolution_clock> begin, uint64_t loops){
 	auto end = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count();
-	double speed = 2.*32.*loops/duration;
+	double speed = 2.*32.*loops*10e6 / (duration * 1048576.);
 	cout << std::dec << loops << " reads done, speed = " << speed <<  " Mbps" << endl;
 }
 
 void test_print(std::chrono::time_point<std::chrono::high_resolution_clock> begin, uint64_t loops, uint32_t block_size){
 	auto end = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count();
-	double speed = 2.*32.*loops*block_size/duration;
+  double speed = (2. * 32. * loops * block_size * 10e6) / (duration * 1048576.) ;
 	cout << std::dec << loops << " reads done, speed = " << speed <<  " Mbps" << endl;
 }
 
@@ -21,7 +21,7 @@ void test_summary(std::chrono::time_point<std::chrono::high_resolution_clock> be
   cout << endl << "Speed test: " << std::dec << loops << " write-reads of " << reg << endl;
   cout << duration << " us total, average : " << duration / loops << " us." << endl;
 
-  double speed = 2.*32.*loops/duration;
+  double speed = 2.*32.*loops*10e6/ (duration * 1048576.);
   double size = 4.*loops / 1048576.;
   cout << "Speed = " << speed << " Mbps" << endl;
   cout << "Size of transfer = " << size << " MB" << endl;
@@ -35,7 +35,7 @@ void test_summary(std::chrono::time_point<std::chrono::high_resolution_clock> be
   cout << endl << "Speed test: " << std::dec << loops << " write-reads of " << reg << " with block size of " << std::dec << block_size << endl;
   cout << duration << " us total, average : " << duration / loops << " us." << endl;
 
-  double speed = 2. * 32. * loops * block_size/duration;
+  double speed = (2. * 32. * loops * block_size * 10e6) / (duration * 1048576.) ;
   double size = 4. * loops * block_size / 1048576.;
   cout << "Speed = " << speed << " Mbps" << endl;
   cout << "Size of transfer = " << size << " MB" << endl;
