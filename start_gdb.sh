2a30fcaff3a638c6d1571a1d96805c8076d8f574
ablaizot <ablaizot@bu.edu>
2024-07-02 18:38:56 +0200

    workaround

--- start_gdb.sh
+++ start_gdb.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-
-gdb -x gdb_commands.txt --args ./test_stand -c 6 
+git pull
+make clean
+make
+./test_stand -c 6 -n ttc.master.common.ctrl
+./test_stand -c 7 -n payload.block_ram.MEM --block_size 10 -l 10000
+#gdb -x gdb_commands.txt --args ./test_stand -c 6 
