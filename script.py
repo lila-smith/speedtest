b5664862456fbde03272d39acda7faacae214130
ablaizot <ablaizot@bu.edu>
2024-07-04 12:50:11 +0200

    write only test

--- script.py
+++ script.py
@@ -2,8 +2,8 @@ import subprocess
 
 # use dictionary to store the different parameters and decrease the loops as the block size goes up
 
-block_sizes = [2**i for i in range(3, 18)]
-loop_numbers = {8: 1000000, 16: 1000000, 32: 1000000, 64: 1000000, 128: 100000, 256: 100000, 512: 100000, 1024: 10000, 2048: 10000, 4096: 10000, 8192: 10000, 16384: 1000, 32768: 1000, 65536: 100, 131072: 100, 262144: 100}
+block_sizes = [2**i for i in range(3, 19)]
+loop_numbers = {8: 1000000, 16: 1000000, 32: 1000000, 64: 1000000, 128: 100000, 256: 100000, 512: 100000, 1024: 10000, 2048: 10000, 4096: 10000, 8192: 10000, 16384: 1000, 32768: 1000, 65536: 500, 131072: 500, 262144: 250}
 fgpas = [1, 2]  # List of different number of FPGAs
 
 # store the output in a dictionary with the key as the FPGA and block size
@@ -14,7 +14,7 @@ def main():
         with open(f'log/F{fpga}.log','w') as f:
             f.write(f"Block_Size, Speed, loops, Transfer Size\n")
             for block_size in block_sizes:
-                command = f"./test_stand -c 7 -n payload.block_ram.MEM --block_size {block_size} -l {loop_numbers[block_size]} -f {fpga} > log/output_f{fpga}_{block_size}.txt"
+                command = f"./test_stand -c 7 -n payload.block_ram.MEM --block_size {block_size} -l {loop_numbers[block_size]} -f {fpga} -w true > log/output_f{fpga}_{block_size}.txt"
                 subprocess.run(command, shell=True)
                 log = open(f"log/output_f{fpga}_{block_size}.txt", "r")
                 output = log.readlines()
