a31d969531ecc023cc54068169f9fd0afd8dcb7d
ablaizot <ablaizot@bu.edu>
2024-07-04 13:51:31 +0200

    new loops

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import subprocess
 # use dictionary to store the different parameters and decrease the loops as the block size goes up
 
 block_sizes = [2**i for i in range(3, 19)]
-loop_numbers = {8: 1000000, 16: 1000000, 32: 1000000, 64: 1000000, 128: 100000, 256: 100000, 512: 100000, 1024: 10000, 2048: 10000, 4096: 10000, 8192: 10000, 16384: 1000, 32768: 1000, 65536: 500, 131072: 500, 262144: 250}
+loop_numbers = {8: 8192000, 16: 4096000, 32: 2048000, 64: 1024000, 128: 512000, 256: 256000, 512: 128000, 1024: 64000, 2048: 32000, 4096: 16000, 8192: 8000, 16384: 4000, 32768: 2000, 65536: 1000, 131072: 500, 262144: 250}
 fgpas = [1, 2]  # List of different number of FPGAs
 
 # store the output in a dictionary with the key as the FPGA and block size
